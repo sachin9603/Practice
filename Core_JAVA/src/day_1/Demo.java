bdf6c7eca089035f41cffbdf8ea2bf947dac4846
sachin9603 <sachinparmar3803@gmail.com>
2025-08-05 17:21:58 +0530

    day2

--- Core_JAVA/src/day_1/Demo.java
+++ Core_JAVA/src/day_1/Demo.java
@@ -1,5 +1,7 @@
 package day_1;
 
+import java.util.Arrays;
+
 public class Demo {
 	
 	// KeyWord
@@ -12,6 +14,112 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my first program ");
+		System.out.println("This is my first program ");
+		System.out.println("This is my first program ");
+		System.out.println("This is my first program ");
+		System.out.println("This is my first program ");
+		System.out.println("This is my first program ");
+		System.out.print("This is my first program ");
+		
+		System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTttt");
+		// print, println
+		
+		
+		int age = 20 ; // varibale name will not start with capital 
+		
+		int sonuKiAge = 30 ; // camel case 
+		
+				// pascal case ---- class name 
+		
+		
+	String mobileNumber  ="9922332244"  ;
+	//int 8 primitive data type ''
+	
+	byte aii  = 23  ;
+	
+	Byte aa  = 23  ;
+	
+	
+	short jk  = 9023 ;
+	
+	int ll  = 999898 ;
+	
+	long  kk  = 323232232 ;
+	
+	char op  = 65 ;
+	
+	
+	
+	float  kkq  = 5.66f ; // 4 byte
+	
+	double lk  = 232323224.3 ; /// 8byte 
+	
+	boolean bbb  = true ; // 1 bit ;
+	
+	//'a'jgkjkjhk
+	
+	
+	/// non-primitive data type 
+	
+	
+	String s  = "sachin" ;
+	
+	System.out.println(s);
+	
+	
+	int mm  = 20  ;
+	int nn  = 30 ;
+	
+    int cc  = mm + nn ;
+    
+    int kl  = 100%3  ;
+    System.out.println(kl); // 1
+    
+    kl++ ; // value of kl will increase by 1  
+    kl--  ; //
+    ++kl  ;
+    --kl ;
+    
+    int q  = 7  ;
+           // 7 + 8
+    int g = q++ + q++  ;
+    System.out.println(g);
+    
+    
+//    int a=10;
+//    System.out.println(a); //10
+//    System.out.println(a++); //10
+//    System.out.println(a);//11 
+//    System.out.println(++a); //12
+//    System.out.println(a--); // 12 ,11 
+//                       //11 - 11
+//    System.out.println(a++ - --a);// 0 ,1,1
+    
+    int a=5;          //4  3      4 -4      2  2        2  2      
+    System.out.println((--a+--a)*(++a-a--)+(--a+a--)*(++a+a++)) ;
+    
+    Byte jkk = 89  ;
+    
+    
+    
+    
+    
+    
+	
+    
+	System.out.println(Byte.MIN_VALUE);
+	
+	
+	int[] arrr = {23,23,23,2,23,23232} ;
+	
+	String[] km  = {"sachin" , "Rahul", "meena"};
+	
+	
+	System.out.println(Arrays.toString(km));
+	
+	
+	
+		
 	
 	}
 	
