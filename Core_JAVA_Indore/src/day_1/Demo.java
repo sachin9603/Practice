a4bbb54c3b79af381e3d53ab5453f970a50ede69
sachin9603 <sachinparmar3803@gmail.com>
2025-09-08 14:57:28 +0530

    added

---
+++ Core_JAVA_Indore/src/day_1/Demo.java
@@ -0,0 +1,21 @@
+package day_1;
+
+// it is a page only 
+public class Demo{
+	
+	
+	// for creating first program 
+	
+	
+	//main+(ctrl+space) then enter 
+	public static void main(String[] args) {
+	
+	
+	    // syso ctrl+space
+		System.out.println("welcome to core java training");
+
+	}
+	
+	
+
+}
